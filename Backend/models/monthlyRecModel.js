061917510e1ef4304035dfc77bd938d43f9bd8fd
Priyanshi Rathore <priyanshirathore624@gmail.com>
2025-08-06 23:12:52 +0530

    Schema design

---
+++ Backend/models/monthlyRecModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const monthlyRecordSchema = new mongoose.Schema({
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User", 
+        required: true 
+    },
+    month:{
+        type: String,
+    },
+    totalCalories: {
+        type: Number,
+    },
+    totalSugar: {
+        type: Number,
+    },
+    totalFiber: {
+        type: Number,
+    },
+    daysTracked: {
+        type: Number,
+    },
+    avgCaloriesPerDay: {
+        type: Number,
+    },
+    avgSugarPerDay: {
+        type: Number,
+    },
+    avgFiberPerDay: {
+        type: Number,
+    }
+});
+
+export default mongoose.model("MonthlyRecord", monthlyRecordSchema);
